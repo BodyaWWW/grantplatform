6159d4807d9eacb7be30413eb785fbba3449b858
hardovsky <b.buhtiyarov@gmail.com>
2024-04-23 14:13:06 +0300

    Моделі 2.0

--- grantpl/grantweb/models.py
+++ grantpl/grantweb/models.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# Модель Соціальних спільнот, або людини, яка відкриває збір
+
 class SocialCommunity(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     tags = models.ManyToManyField('Tag', related_name='communities')
+    avatar = models.ImageField
 
     def __str__(self):
         return self.name
+
+#Модель Грантів
 
 class Grant(models.Model):
     name = models.CharField(max_length=100)
@@ -18,6 +23,8 @@ class Grant(models.Model):
     def __str__(self):
         return self.name
 
+#Модель Донатів
+
 class Donation(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -27,15 +34,31 @@ class Donation(models.Model):
     def __str__(self):
         return self.name
 
+#Модель тегів по яким будуть розроблені фільтри
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+#Модель людей які будуть донатити
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     interests = models.ManyToManyField('Tag', related_name='users')
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.user.username
+
+#Модель організацій які надають гранти
+
+class GrantOrg(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    website = models.URLField()
+    contact_email = models.EmailField()
+
+    def __str__(self):
+        return self.name
